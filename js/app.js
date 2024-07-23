1a867a8da5a4f534c3c5424a05c8ada65a8b4da6
Christian Fernandez <christian14@gmail.com>
2024-07-23 17:47:04 -0400

    Ocultando la alerta, si pasa la validacion / ojo que el campo no este vacio / .135

--- js/app.js
+++ js/app.js
@@ -14,31 +14,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
     if (valorInput.trim() === '') {
       mostrarAlerta(`El campo ${event.target.id} es obligatorio`, valorReferencia);
+      return;
 
-    } else {
-      console.log(valorInput.trim());
     }
 
-  };
+    limpiarAlerta(valorReferencia);
+    // Para verificar en consola que si entra al if de arriba, no se ejecuta la linea 21
+    // console.log('despues del if');
+
+  }
 
   function mostrarAlerta(mensaje, referencia) {
-    // Comprobamos si existe un alerta, pero no nos vamos al document, sino a la referencia
-    // const existeAlerta = document.querySelector('.alert'); // Aca buscaba en todo el documentos
-    const existeAlerta = referencia.querySelector('.alert');  // aca solo en el input que queremos
 
-    console.log(referencia);
-    console.log(existeAlerta);
-
-    if (existeAlerta) {
-      existeAlerta.remove()
-    }
+    limpiarAlerta(referencia);
 
     const error = document.createElement('P');
     error.textContent = mensaje;
     error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center', 'alert');
     referencia.appendChild(error);
 
-  };
+  }
+
+  function limpiarAlerta(referencia) {
+    // console.log('Desde limpiar alerta...');
+
+    const existeAlerta = referencia.querySelector('.alert');
+
+    if (existeAlerta) {
+      existeAlerta.remove()
+    }
+  }
 
 });
 
@@ -52,4 +57,8 @@ document.addEventListener('DOMContentLoaded', function () {
  * 2.- Para solucionar lo anterior tenemos que fijarnos que nos muestra el elemento alerta, y podemos usar unas de sus clases, para seleccionarla y condicionarla
  * 
  * 3.- Nos encontramos que habiamos incluido una class de nombre alert, y a ella apuntamos, haciendo referencia al elemento que viene del segundo parametro de la funcion que estoy trabajando, ya que si tomamos todo el documento, nos borraria al alerta previa si haberla corregido por la validacion que le pusimos con el "if"
+ * 
+ * 4.- El return interno del if, nos ayuda a cortar la ejecucion y que el codigo no siga ejecutando.
+ * 
+ * 5.- Viendo que el codigo que esta en "mostrarAlerta", es lo que debemos hacer en "validar", es momento de crear una funcion que se encargue de es logica.
  */
