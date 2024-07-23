9df8b8dbc34a7dac53810541f5d94022e76cc246
Christian Fernandez <christian14@gmail.com>
2024-07-22 22:05:57 -0400

    Prevenir que se generen multiples alerts / el uso de la referencia actual / .134

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
-
   const inputEmail = document.querySelector('#email');
   const inputAsunto = document.querySelector('#asunto');
   const inputMensaje = document.querySelector('#mensaje');
@@ -10,11 +9,8 @@ document.addEventListener('DOMContentLoaded', function () {
   inputMensaje.addEventListener('blur', validar);
 
   function validar(event) {
-    const valorInput = event.target.value
-
-    const valorReferencia = event.target.parentElement
-    // Haciendo algo de traversing
-    console.log(event.target.parentElement);
+    const valorInput = event.target.value;
+    const valorReferencia = event.target.parentElement;
 
     if (valorInput.trim() === '') {
       mostrarAlerta(`El campo ${event.target.id} es obligatorio`, valorReferencia);
@@ -25,13 +21,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
   };
 
-  // Le pasamos la referencia como otro parametro
   function mostrarAlerta(mensaje, referencia) {
+    // Comprobamos si existe un alerta, pero no nos vamos al document, sino a la referencia
+    // const existeAlerta = document.querySelector('.alert'); // Aca buscaba en todo el documentos
+    const existeAlerta = referencia.querySelector('.alert');  // aca solo en el input que queremos
+
+    console.log(referencia);
+    console.log(existeAlerta);
+
+    if (existeAlerta) {
+      existeAlerta.remove()
+    }
+
     const error = document.createElement('P');
     error.textContent = mensaje;
-    error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center')
-    // Dejamos de apuntar al formulario y nos fijamos de la referencia.
-    referencia.appendChild(error)
+    error.classList.add('bg-red-600', 'text-white', 'p-2', 'text-center', 'alert');
+    referencia.appendChild(error);
 
   };
 
@@ -42,8 +47,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
 /**Comentarios extras:
  * 
- * 1.- Hasta ahora los mensajes de alerta, no estan sino hasta al final del formulario, estos es producto del appendChild que tiene.
+ * 1.- Ahora el problema esta asociado a que el mensaje de alerta sale multiple veces y lo que queremos que salga una sola vez, cuando no se cumpla la condición.
  * 
- * 2.- Hcaemos traversing al docuemento, hasta ubicar donde queremos posicionarnos, para de luego dejar de apuntar al formulario completo y solo enfoncarnos en el elemento donde queremos inyectar el mensaje
+ * 2.- Para solucionar lo anterior tenemos que fijarnos que nos muestra el elemento alerta, y podemos usar unas de sus clases, para seleccionarla y condicionarla
  * 
+ * 3.- Nos encontramos que habiamos incluido una class de nombre alert, y a ella apuntamos, haciendo referencia al elemento que viene del segundo parametro de la funcion que estoy trabajando, ya que si tomamos todo el documento, nos borraria al alerta previa si haberla corregido por la validacion que le pusimos con el "if"
  */
